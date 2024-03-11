cd6b0b7dfddab015595cfd4746c5db24409b548e
Jim Salyer <jim.salyer@efleets.com>
2024-03-11 10:36:30 -0500

    Add remining unit tests. JS

--- jira/view-issue.test.js
+++ jira/view-issue.test.js
@@ -1,54 +1,66 @@
-const defaultWindowLocation = window.location;
+describe('Jira - View Issue', () => {
+  const defaultWindowLocation = window.location;
 
-const alertMock = jest.fn();
-const assignMock = jest.fn();
-const promptMock = jest.fn();
+  const alertMock = jest.fn();
+  const assignMock = jest.fn();
+  const promptMock = jest.fn();
 
-beforeEach(() => {
-  delete window.location;
+  beforeEach(() => {
+    delete window.location;
 
-  window.location = {
-    ...defaultWindowLocation,
-    assign: assignMock,
-  };
+    window.location = {
+      ...defaultWindowLocation,
+      assign: assignMock,
+    };
 
-  window.alert = alertMock;
-  window.prompt = promptMock;
-});
+    window.alert = alertMock;
+    window.prompt = promptMock;
+  });
 
-afterEach(() => {
-  window.location = defaultWindowLocation;
-});
+  afterEach(() => {
+    window.location = defaultWindowLocation;
+  });
 
-it('navigates to the Jira issue entered in the prompt', () => {
-  const issue = 'test-1';
+  it('navigates to the Jira issue entered in the prompt', () => {
+    const issue = 'test-1';
 
-  promptMock.mockReturnValue(issue);
+    promptMock.mockReturnValue(issue);
 
-  require('./view-issue');
+    require('./view-issue');
 
-  expect(promptMock).toHaveBeenCalledWith('Enter issue to view');
-  expect(assignMock).toHaveBeenCalledWith(
-    `https://jira.efleets.com/browse/${issue}`,
-  );
-});
+    expect(promptMock).toHaveBeenCalledWith('Enter issue to view');
+    expect(assignMock).toHaveBeenCalledWith(
+      `https://jira.efleets.com/browse/${issue}`,
+    );
+  });
 
-it('presents an error and returns if no issue is entered in the prompt', () => {
-  promptMock.mockReturnValue('');
+  it('presents an error and returns if no issue is entered in the prompt', () => {
+    promptMock.mockReturnValue('');
 
-  require('./view-issue');
+    require('./view-issue');
 
-  expect(alertMock).toHaveBeenCalledWith('You must enter an issue to view.');
-  expect(assignMock).not.toHaveBeenCalled();
-});
+    expect(alertMock).toHaveBeenCalledWith('You must enter an issue to view.');
+    expect(assignMock).not.toHaveBeenCalled();
+  });
 
-it('presents and error and returns if an invalid issue has been entered in the prompt', () => {
-  promptMock.mockReturnValue('invalid');
+  it('presents and error and returns if an invalid issue has been entered in the prompt', () => {
+    promptMock.mockReturnValue('invalid');
 
-  require('./view-issue');
+    require('./view-issue');
 
-  expect(alertMock).toHaveBeenCalledWith(
-    'You must enter a valid issue to view.',
-  );
-  expect(assignMock).not.toHaveBeenCalled();
+    expect(alertMock).toHaveBeenCalledWith(
+      'You must enter a valid issue to view.',
+    );
+    expect(assignMock).not.toHaveBeenCalled();
+  });
+
+  it('prepends project portion of issue if missing', () => {
+    promptMock.mockReturnValue('1');
+
+    require('./view-issue');
+
+    expect(assignMock).toHaveBeenCalledWith(
+      `https://jira.efleets.com/browse/EDGE-1`,
+    );
+  });
 });
